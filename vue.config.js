1f603e110164d768cbc1b0b1e7be38b942468625
zpzhang <chee7@qq.com>
2021-08-16 14:52:13 +0800

    [master] 修改

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,11 @@
-const path = require('path')
+const DEV_CONFIG = {
+  pages: {
+    index: {
+      entry: 'examples/main.js'
+    }
+  }
+};
+const BUILD_CONFIG = {
 
-function resolve(dir) {
-    return path.join(__dirname, dir)
-}
-
-module.exports = {
-    configureWebpack: {
-        entry: {
-            app: './examples/main.js'
-        },
-        externals: {
-            vue: {
-                root: 'Vue',
-                commonjs: 'vue',
-                commonjs2: 'vue',
-                amd: 'vue'
-            }
-        },
-        // module: {
-        //     rules: [{
-        //         test: /\.js$/,
-        //         loader: "babel-loader",
-        //         include: [resolve('examples'), resolve('packages')]
-        //     }]
-        // }
-    },
-    // chainWebpack: (config) => {
-    //     config.module.rule("compile")
-    //         .test(/\.js$/)
-    //         .include.add(resolve('examples'))
-    //         .loader('babelLoader').use('babel-loader')
-    // }
-}
+};
+module.exports = process.env.NODE_ENV === 'development' ? DEV_CONFIG : BUILD_CONFIG;
